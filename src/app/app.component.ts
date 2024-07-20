65830b1f4a15df48493c51501e9f8c7d524daa67
Julien Salvatore <julien.salvatore@gmail.com>
2024-07-20 17:53:36 +0200

    boostrapp with module

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,16 +1,10 @@
 import { Component } from '@angular/core';
-
-import { HeaderComponent } from './header/header.component';
-import { UserComponent } from './user/user.component';
 import { DUMMY_USERS } from './dummy-users';
-import { TasksComponent } from "./tasks/tasks.component";
 
 @Component({
-    selector: 'app-root',
-    standalone: true,
-    templateUrl: './app.component.html',
-    styleUrl: './app.component.css',
-    imports: [HeaderComponent, UserComponent, TasksComponent]
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrl: './app.component.css',
 })
 export class AppComponent {
   users = DUMMY_USERS;
--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,5 @@
-import { bootstrapApplication } from '@angular/platform-browser';
+import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 
-import { AppComponent } from './app/app.component';
+import { AppModule } from './app/app.module';
 
-bootstrapApplication(AppComponent).catch((err) => console.error(err));
+platformBrowserDynamic().bootstrapModule(AppModule);
